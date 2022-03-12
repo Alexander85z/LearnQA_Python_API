53d775330c2cb04b6b6432cde78c20ed0bdb542a
Alexander85z <89161814170@mail.ru>
2022-03-12 20:58:28 +0300

    Lesson 11

--- test_examples.py
+++ test_examples.py
@@ -1,4 +1,11 @@
+import requests
+
+
 class TestExample:
-    def test_len_name(self):
-        len_name= input("Введите фразу не больше 15 символов:")
-        assert len(len_name) < 15
+    def test_cookie(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        print(dict(response.cookies))
+        cookies_key = response.cookies.keys()
+        cookies_value = response.cookies.values()
+        assert cookies_key == cookies_key
+        assert cookies_value == cookies_value

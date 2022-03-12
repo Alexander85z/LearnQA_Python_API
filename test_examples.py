265dc0eafd8e32213d3cf5341ff97707dd0799bd
Alexander85z <89161814170@mail.ru>
2022-03-12 21:45:48 +0300

    Lesson 11

--- test_examples.py
+++ test_examples.py
@@ -5,7 +5,6 @@ class TestExample:
     def test_cookie(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         print(dict(response.cookies))
-        cookies_key = response.cookies.keys()
+
         cookies_value = response.cookies.values()
-        assert cookies_key == cookies_key
-        assert cookies_value == cookies_value
+        assert cookies_value == ['hw_value'] , f"Неверный куки"
